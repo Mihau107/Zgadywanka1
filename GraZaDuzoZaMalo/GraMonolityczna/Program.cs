d59856be90f82ffe8f896790053bcc2c821fdbf3
Mihau107 <48652209+Mihau107@users.noreply.github.com>
2019-03-17 17:13:40 +0100

    Wersja 1

---
+++ GraZaDuzoZaMalo/GraMonolityczna/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolityczna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+            Console.Write("Podaj swoje imię proszę: ");
+            string imie = Console.ReadLine();
+
+            Console.WriteLine($"Cześć {imie}!");
+        }
+    }
+}
