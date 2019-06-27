84e307f4faba10a86a19fbfbe66f1fa595f4a72f
Mihau107 <48652209+Mihau107@users.noreply.github.com>
2019-06-27 18:08:08 +0200

    Graficzny interfejs
    Poprawiony kod, Gui powinno działać jak należy

--- GraZaDuzoZaMalo/GraGUI/Form1.Designer.cs
+++ GraZaDuzoZaMalo/GraGUI/Form1.Designer.cs
@@ -28,107 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnNowaGra = new System.Windows.Forms.Button();
-            this.grpBoxLosowanie = new System.Windows.Forms.GroupBox();
-            this.lblZakresOd = new System.Windows.Forms.Label();
-            this.lblZakresDo = new System.Windows.Forms.Label();
-            this.textBoxOd = new System.Windows.Forms.TextBox();
-            this.textBoxDo = new System.Windows.Forms.TextBox();
-            this.btnLosuj = new System.Windows.Forms.Button();
-            this.grpBoxLosowanie.SuspendLayout();
+            this.NowaGra = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Losuj = new System.Windows.Forms.Button();
+            this.textBoxdo = new System.Windows.Forms.TextBox();
+            this.ZakresDo = new System.Windows.Forms.Label();
+            this.textBoxod = new System.Windows.Forms.TextBox();
+            this.ZakresOd = new System.Windows.Forms.Label();
+            this.PodanaLiczba = new System.Windows.Forms.TextBox();
+            this.Sprawdz_Click = new System.Windows.Forms.Button();
+            this.Sprawdzanie_Box = new System.Windows.Forms.GroupBox();
+            this.PodajLiczbe = new System.Windows.Forms.Label();
+            this.komunikat = new System.Windows.Forms.Label();
+            this.Poddanie = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.Sprawdzanie_Box.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnNowaGra
+            // NowaGra
             // 
-            this.btnNowaGra.Location = new System.Drawing.Point(13, 13);
-            this.btnNowaGra.Name = "btnNowaGra";
-            this.btnNowaGra.Size = new System.Drawing.Size(75, 23);
-            this.btnNowaGra.TabIndex = 0;
-            this.btnNowaGra.Text = "Nowa gra";
-            this.btnNowaGra.UseVisualStyleBackColor = true;
-            this.btnNowaGra.Click += new System.EventHandler(this.btnNowaGra_Click);
+            this.NowaGra.Location = new System.Drawing.Point(13, 13);
+            this.NowaGra.Name = "NowaGra";
+            this.NowaGra.Size = new System.Drawing.Size(75, 23);
+            this.NowaGra.TabIndex = 0;
+            this.NowaGra.Text = "NowaGra";
+            this.NowaGra.UseVisualStyleBackColor = true;
+            this.NowaGra.Click += new System.EventHandler(this.NowaGra_Click);
             // 
-            // grpBoxLosowanie
+            // groupBox1
             // 
-            this.grpBoxLosowanie.Controls.Add(this.btnLosuj);
-            this.grpBoxLosowanie.Controls.Add(this.textBoxDo);
-            this.grpBoxLosowanie.Controls.Add(this.textBoxOd);
-            this.grpBoxLosowanie.Controls.Add(this.lblZakresDo);
-            this.grpBoxLosowanie.Controls.Add(this.lblZakresOd);
-            this.grpBoxLosowanie.Location = new System.Drawing.Point(13, 52);
-            this.grpBoxLosowanie.Name = "grpBoxLosowanie";
-            this.grpBoxLosowanie.Size = new System.Drawing.Size(287, 121);
-            this.grpBoxLosowanie.TabIndex = 1;
-            this.grpBoxLosowanie.TabStop = false;
-            this.grpBoxLosowanie.Text = "Losowanie";
-            this.grpBoxLosowanie.Visible = false;
+            this.groupBox1.Controls.Add(this.Losuj);
+            this.groupBox1.Controls.Add(this.textBoxdo);
+            this.groupBox1.Controls.Add(this.ZakresDo);
+            this.groupBox1.Controls.Add(this.textBoxod);
+            this.groupBox1.Controls.Add(this.ZakresOd);
+            this.groupBox1.Location = new System.Drawing.Point(13, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Visible = false;
+            this.groupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
             // 
-            // lblZakresOd
+            // Losuj
             // 
-            this.lblZakresOd.AutoSize = true;
-            this.lblZakresOd.Location = new System.Drawing.Point(23, 28);
-            this.lblZakresOd.Name = "lblZakresOd";
-            this.lblZakresOd.Size = new System.Drawing.Size(58, 13);
-            this.lblZakresOd.TabIndex = 0;
-            this.lblZakresOd.Text = "Zakres od:";
+            this.Losuj.Location = new System.Drawing.Point(205, 59);
+            this.Losuj.Name = "Losuj";
+            this.Losuj.Size = new System.Drawing.Size(75, 23);
+            this.Losuj.TabIndex = 2;
+            this.Losuj.Text = "Wylosuj";
+            this.Losuj.UseVisualStyleBackColor = true;
+            this.Losuj.Click += new System.EventHandler(this.Losuj_Click);
             // 
-            // lblZakresDo
+            // textBoxdo
             // 
-            this.lblZakresDo.AutoSize = true;
-            this.lblZakresDo.Location = new System.Drawing.Point(23, 77);
-            this.lblZakresDo.Name = "lblZakresDo";
-            this.lblZakresDo.Size = new System.Drawing.Size(58, 13);
-            this.lblZakresDo.TabIndex = 1;
-            this.lblZakresDo.Text = "Zakres do:";
+            this.textBoxdo.Location = new System.Drawing.Point(70, 59);
+            this.textBoxdo.Name = "textBoxdo";
+            this.textBoxdo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxdo.TabIndex = 3;
             // 
-            // textBoxOd
+            // ZakresDo
             // 
-            this.textBoxOd.Location = new System.Drawing.Point(87, 25);
-            this.textBoxOd.Name = "textBoxOd";
-            this.textBoxOd.Size = new System.Drawing.Size(100, 20);
-            this.textBoxOd.TabIndex = 2;
+            this.ZakresDo.AutoSize = true;
+            this.ZakresDo.Location = new System.Drawing.Point(7, 59);
+            this.ZakresDo.Name = "ZakresDo";
+            this.ZakresDo.Size = new System.Drawing.Size(57, 13);
+            this.ZakresDo.TabIndex = 2;
+            this.ZakresDo.Text = "Zakres Do";
             // 
-            // textBoxDo
+            // textBoxod
             // 
-            this.textBoxDo.Location = new System.Drawing.Point(87, 74);
-            this.textBoxDo.Name = "textBoxDo";
-            this.textBoxDo.Size = new System.Drawing.Size(100, 20);
-            this.textBoxDo.TabIndex = 3;
+            this.textBoxod.Location = new System.Drawing.Point(70, 20);
+            this.textBoxod.Name = "textBoxod";
+            this.textBoxod.Size = new System.Drawing.Size(100, 20);
+            this.textBoxod.TabIndex = 1;
             // 
-            // btnLosuj
+            // ZakresOd
             // 
-            this.btnLosuj.Location = new System.Drawing.Point(206, 46);
-            this.btnLosuj.Name = "btnLosuj";
-            this.btnLosuj.Size = new System.Drawing.Size(75, 23);
-            this.btnLosuj.TabIndex = 2;
-            this.btnLosuj.Text = "Wylosuj";
-            this.btnLosuj.UseVisualStyleBackColor = true;
-            this.btnLosuj.Click += new System.EventHandler(this.btnLosuj_Click);
+            this.ZakresOd.AutoSize = true;
+            this.ZakresOd.Location = new System.Drawing.Point(7, 20);
+            this.ZakresOd.Name = "ZakresOd";
+            this.ZakresOd.Size = new System.Drawing.Size(57, 13);
+            this.ZakresOd.TabIndex = 0;
+            this.ZakresOd.Text = "Zakres Od";
+            // 
+            // PodanaLiczba
+            // 
+            this.PodanaLiczba.Location = new System.Drawing.Point(77, 46);
+            this.PodanaLiczba.Name = "PodanaLiczba";
+            this.PodanaLiczba.Size = new System.Drawing.Size(100, 20);
+            this.PodanaLiczba.TabIndex = 2;
+            this.PodanaLiczba.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Sprawdz_Click
+            // 
+            this.Sprawdz_Click.Location = new System.Drawing.Point(205, 84);
+            this.Sprawdz_Click.Name = "Sprawdz_Click";
+            this.Sprawdz_Click.Size = new System.Drawing.Size(75, 23);
+            this.Sprawdz_Click.TabIndex = 3;
+            this.Sprawdz_Click.Text = "Sprawdź";
+            this.Sprawdz_Click.UseVisualStyleBackColor = true;
+            this.Sprawdz_Click.Click += new System.EventHandler(this.Sprawdz_Click_Click);
+            // 
+            // Sprawdzanie_Box
+            // 
+            this.Sprawdzanie_Box.Controls.Add(this.komunikat);
+            this.Sprawdzanie_Box.Controls.Add(this.PodajLiczbe);
+            this.Sprawdzanie_Box.Controls.Add(this.Sprawdz_Click);
+            this.Sprawdzanie_Box.Controls.Add(this.PodanaLiczba);
+            this.Sprawdzanie_Box.Location = new System.Drawing.Point(13, 170);
+            this.Sprawdzanie_Box.Name = "Sprawdzanie_Box";
+            this.Sprawdzanie_Box.Size = new System.Drawing.Size(297, 196);
+            this.Sprawdzanie_Box.TabIndex = 4;
+            this.Sprawdzanie_Box.TabStop = false;
+            this.Sprawdzanie_Box.Text = "Sprawdzanie";
+            this.Sprawdzanie_Box.Visible = false;
+            this.Sprawdzanie_Box.Enter += new System.EventHandler(this.Sprawdzanie_Box_Enter);
+            // 
+            // PodajLiczbe
+            // 
+            this.PodajLiczbe.AutoSize = true;
+            this.PodajLiczbe.Location = new System.Drawing.Point(7, 53);
+            this.PodajLiczbe.Name = "PodajLiczbe";
+            this.PodajLiczbe.Size = new System.Drawing.Size(64, 13);
+            this.PodajLiczbe.TabIndex = 4;
+            this.PodajLiczbe.Text = "Podaj liczbę";
+            this.PodajLiczbe.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // komunikat
+            // 
+            this.komunikat.AutoSize = true;
+            this.komunikat.ForeColor = System.Drawing.Color.Red;
+            this.komunikat.Location = new System.Drawing.Point(135, 156);
+            this.komunikat.Name = "komunikat";
+            this.komunikat.Size = new System.Drawing.Size(35, 13);
+            this.komunikat.TabIndex = 6;
+            this.komunikat.Text = "label2";
+            this.komunikat.Visible = false;
+            // 
+            // Poddanie
+            // 
+            this.Poddanie.Location = new System.Drawing.Point(195, 473);
+            this.Poddanie.Name = "Poddanie";
+            this.Poddanie.Size = new System.Drawing.Size(98, 47);
+            this.Poddanie.TabIndex = 5;
+            this.Poddanie.Text = "Poddaj się";
+            this.Poddanie.UseVisualStyleBackColor = true;
+            this.Poddanie.Visible = false;
+            this.Poddanie.Click += new System.EventHandler(this.Poddanie_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 362);
-            this.Controls.Add(this.grpBoxLosowanie);
-            this.Controls.Add(this.btnNowaGra);
+            this.ClientSize = new System.Drawing.Size(348, 532);
+            this.Controls.Add(this.Poddanie);
+            this.Controls.Add(this.Sprawdzanie_Box);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.NowaGra);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.grpBoxLosowanie.ResumeLayout(false);
-            this.grpBoxLosowanie.PerformLayout();
+            this.Text = "Gra Za Dużo Za Mało";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Sprawdzanie_Box.ResumeLayout(false);
+            this.Sprawdzanie_Box.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnNowaGra;
-        private System.Windows.Forms.GroupBox grpBoxLosowanie;
-        private System.Windows.Forms.Button btnLosuj;
-        private System.Windows.Forms.TextBox textBoxDo;
-        private System.Windows.Forms.TextBox textBoxOd;
-        private System.Windows.Forms.Label lblZakresDo;
-        private System.Windows.Forms.Label lblZakresOd;
+        private System.Windows.Forms.Button NowaGra;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxdo;
+        private System.Windows.Forms.Label ZakresDo;
+        private System.Windows.Forms.TextBox textBoxod;
+        private System.Windows.Forms.Label ZakresOd;
+        private System.Windows.Forms.Button Losuj;
+        private System.Windows.Forms.TextBox PodanaLiczba;
+        private System.Windows.Forms.Button Sprawdz_Click;
+        private System.Windows.Forms.GroupBox Sprawdzanie_Box;
+        private System.Windows.Forms.Label PodajLiczbe;
+        private System.Windows.Forms.Label komunikat;
+        private System.Windows.Forms.Button Poddanie;
     }
 }
 
--- GraZaDuzoZaMalo/GraGUI/Form1.cs
+++ GraZaDuzoZaMalo/GraGUI/Form1.cs
@@ -8,34 +8,148 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ModelGry;
-
 namespace GraGUI
 {
     public partial class Form1 : Form
     {
-        private Gra g;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnNowaGra_Click(object sender, EventArgs e)
+        private Gra g;
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            grpBoxLosowanie.Visible = true;
-            btnNowaGra.Enabled = false;
+
         }
 
-        private void btnLosuj_Click(object sender, EventArgs e)
+        private void NowaGra_Click(object sender, EventArgs e)
         {
-            // todo: try-catch
-            int a = int.Parse(textBoxOd.Text);
-            int b = int.Parse(textBoxDo.Text);
-            g = new Gra(a, b);
+            groupBox1.Visible = true;
+            NowaGra.Enabled = false;
+        }
 
-            textBoxOd.Enabled = false;
-            textBoxDo.Enabled = false;
-            btnLosuj.Enabled = false;
+
+        private void Losuj_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            string test = textBoxod.Text;
+            string test2 = textBoxdo.Text;
+
+            if(string.IsNullOrEmpty(test) || string.IsNullOrEmpty(test2))
+            {
+                MessageBox.Show("Musisz podać poprawny zakres");
+            }else
+            {
+                int a = int.Parse(textBoxod.Text);
+                int b = int.Parse(textBoxdo.Text);
+                g = new Gra(a, b);
+                textBoxod.Enabled = false;
+                textBoxdo.Enabled = false;
+                Losuj.Visible = false;
+                Sprawdzanie_Box.Visible = true;
+                Poddanie.Visible = true;
+            }
+
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Poddanie_Click(object sender, EventArgs e)
+        {
+
+            string message = "Przegrałeś... Grasz jeszcze raz?";
+            string title = "PRZEGRANA";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.No)
+            {
+                this.Close();
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                NowaGra.Enabled = true;
+                textBoxdo.Clear();
+                textBoxod.Clear();
+                PodanaLiczba.Clear();
+                textBoxod.Enabled = true;
+                textBoxdo.Enabled = true;
+                groupBox1.Visible = false;
+                Losuj.Visible = true;
+                Sprawdzanie_Box.Visible = false;
+            }
+
+
+        }
+
+        private void Sprawdz_Click_Click(object sender, EventArgs e)
+        {
+            komunikat.Visible = true;
+
+            string test3 = PodanaLiczba.Text;
+            if (string.IsNullOrEmpty(test3))
+            {
+                MessageBox.Show("Podaj swoją propozycję");
+            }
+            else
+            {
+                int c = int.Parse(PodanaLiczba.Text);
+                if (Convert.ToString(g.Ocena(c)) == "ZaMalo")
+                    komunikat.Text = "Źle, za mało";
+                else if (Convert.ToString(g.Ocena(c)) == "ZaDuzo")
+                    komunikat.Text = "Źle, za dużo";
+                else
+                {
+                    string message = $"Brawo! Wygrałeś! Grasz jeszcze raz? \n Ilość Ruchów: {Convert.ToString(g.LicznikRuchow - 1)}\n";
+                    string title = "WYGRANA";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result = MessageBox.Show(message, title, buttons);
+                    if (result == DialogResult.No)
+                    {
+                        this.Close();
+                    }
+                    else
+                    {
+                        NowaGra.Enabled = true;
+                        textBoxdo.Clear();
+                        textBoxod.Clear();
+                        PodanaLiczba.Clear();
+                        textBoxod.Enabled = true;
+                        textBoxdo.Enabled = true;
+                        groupBox1.Visible = false;
+                        Losuj.Visible = true;
+                        Sprawdzanie_Box.Visible = false;
+                    }
+                }
+            }
+        }
+
+        private void Sprawdzanie_Box_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
+
